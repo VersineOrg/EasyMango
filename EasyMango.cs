7e864a9f7b4c7b5a675237e5fcaaaaa3701a2ecf
rayan <rayan@pop-os.localdomain>
2022-03-06 02:01:37 +0100

    added EasyMango class

---
+++ EasyMango.cs
@@ -0,0 +1,128 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace EasyMango;
+
+public class EasyMango
+{
+    private IMongoCollection<BsonDocument> collection;
+    
+    public EasyMango(string connectionString, string databaseName, string collectionName)
+    {
+        // Connect to the MongoDB Database
+        MongoClient client = new MongoClient(connectionString);
+        IMongoDatabase database = client.GetDatabase(databaseName);
+        collection = database.GetCollection<BsonDocument>(collectionName);
+    }
+
+    public bool GetSingleDatabaseEntry(string field, string value, out BsonDocument result)
+    {
+        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+        
+        result = collection.Find(filter).FirstOrDefault();
+
+        return (result != null);
+    }
+    public bool GetMultipleDatabaseEntries(string field, string value, out List<BsonDocument> result)
+    {
+        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+        result = collection.Find(filter).ToList();
+
+        return (result != null);
+    }
+    
+    public bool ReplaceSingleDatabaseEntry(string field, string value, BsonDocument entry)
+    {
+        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+        try
+        {
+            collection.FindOneAndReplace(filter, entry);
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool ReplaceMultipleDatabaseEntries(string field, string value, BsonDocument entry)
+    {
+        List<BsonDocument> result = new List<BsonDocument>();
+        if (!GetMultipleDatabaseEntries(field, value, out result))
+        {
+            return false;
+        }
+        try
+        {
+            foreach (BsonDocument document in result)
+            {
+                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", document.GetValue("_id"));
+                collection.FindOneAndReplace(filter, entry);
+            }
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool AddSingleDatabaseEntry(BsonDocument entry)
+    {
+        try
+        {
+            collection.InsertOne(entry);
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool AddMultipleDatabaseEntries(List<BsonDocument> entries)
+    {
+        try
+        {
+            collection.InsertMany(entries);
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    public bool RemoveSingleDatabaseEntry(string field, string value)
+    {
+        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+        try
+        {
+            collection.FindOneAndDelete(filter);
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool RemoveMultiplesDatabaseEntries(string field, string value)
+    {
+        List<BsonDocument> result = new List<BsonDocument>();
+        if (!GetMultipleDatabaseEntries(field, value, out result))
+        {
+            return false;
+        }
+        try
+        {
+            foreach (BsonDocument document in result)
+            {
+                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", document.GetValue("_id"));
+                collection.FindOneAndDelete(filter);
+            }
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+}
