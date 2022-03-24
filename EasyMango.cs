843f427f22f0e8c225fdac5ab88f0790af71fd51
rayan <rayan@pop-os.localdomain>
2022-03-24 11:23:47 +0100

    change .net framework version to 5.0

--- EasyMango.cs
+++ EasyMango.cs
@@ -1,170 +1,190 @@
 using MongoDB.Bson;
 using MongoDB.Driver;
 
-namespace EasyMango;
-
-public class EasyMango
+namespace EasyMango
 {
-    private readonly IMongoCollection<BsonDocument> collection;
-    
-    public enum SortingOrder
+    public class EasyMango
     {
-        Default,
-        Ascending,
-        Descending
-    }
-    
-    public EasyMango(string connectionString, string databaseName, string collectionName)
-    {
-        // Connect to the MongoDB Database
-        MongoClient client = new MongoClient(connectionString);
-        IMongoDatabase database = client.GetDatabase(databaseName);
-        collection = database.GetCollection<BsonDocument>(collectionName);
-    }
+        private readonly IMongoCollection<BsonDocument> collection;
 
-    public bool GetSingleDatabaseEntry<T>(string field, T value, out BsonDocument result)
-    {
-        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
-        
-        result = collection.Find(filter).FirstOrDefault();
-
-        return (result != null);
-    }
-    public bool GetSingleDatabaseEntry<T>(string field, T value,SortingOrder sortingOrder,string sortingField, out BsonDocument result)
-    {
-        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
-
-        switch (sortingOrder)
+        public enum SortingOrder
         {
-            case SortingOrder.Ascending:
-                result = collection.Find(filter).SortBy(bson => bson[sortingField]).FirstOrDefault();
-                break;
-            case SortingOrder.Descending: 
-                result = collection.Find(filter).SortByDescending(bson => bson[sortingField]).FirstOrDefault();
-                break;
-            default:
-                GetSingleDatabaseEntry(field, value, out result);
-                break;
+            Default,
+            Ascending,
+            Descending
         }
 
-        return (result != null);
-    }
-    public bool GetMultipleDatabaseEntries<T>(string field, T value, out List<BsonDocument> result)
-    {
-        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
-        result = collection.Find(filter).ToList();
-
-        return (result != null);
-    }
-    public bool GetMultipleDatabaseEntries<T>(string field, T value,SortingOrder sortingOrder, string sortingField, out List<BsonDocument> result)
-    {
-        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
-        
-        switch (sortingOrder)
+        public EasyMango(string connectionString, string databaseName, string collectionName)
         {
-            case SortingOrder.Ascending:
-                result = collection.Find(filter).SortBy(bson => bson[sortingField]).ToList();
-                break;
-            case SortingOrder.Descending: 
-                result = collection.Find(filter).SortByDescending(bson => bson[sortingField]).ToList();
-                break;
-            default:
-                GetMultipleDatabaseEntries(field, value, out result);
-                break;
+            // Connect to the MongoDB Database
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase database = client.GetDatabase(databaseName);
+            collection = database.GetCollection<BsonDocument>(collectionName);
         }
 
-        return (result != null);
-    }
-    public bool ReplaceSingleDatabaseEntry<T>(string field, T value, BsonDocument entry)
-    {
-        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
-        try
+        public bool GetSingleDatabaseEntry<T>(string field, T value, out BsonDocument result)
         {
-            collection.FindOneAndReplace(filter, entry);
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+
+            result = collection.Find(filter).FirstOrDefault();
+
+            return (result != null);
         }
-        catch
+
+        public bool GetSingleDatabaseEntry<T>(string field, T value, SortingOrder sortingOrder, string sortingField,
+            out BsonDocument result)
         {
-            return false;
-        }
-        return true;
-    }
-    public bool ReplaceMultipleDatabaseEntries<T>(string field, T value, BsonDocument entry)
-    {
-        if (!GetMultipleDatabaseEntries(field, value, out List<BsonDocument> result))
-        {
-            return false;
-        }
-        try
-        {
-            foreach (BsonDocument document in result)
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+
+            switch (sortingOrder)
             {
-                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", document.GetValue("_id"));
+                case SortingOrder.Ascending:
+                    result = collection.Find(filter).SortBy(bson => bson[sortingField]).FirstOrDefault();
+                    break;
+                case SortingOrder.Descending:
+                    result = collection.Find(filter).SortByDescending(bson => bson[sortingField]).FirstOrDefault();
+                    break;
+                default:
+                    GetSingleDatabaseEntry(field, value, out result);
+                    break;
+            }
+
+            return (result != null);
+        }
+
+        public bool GetMultipleDatabaseEntries<T>(string field, T value, out List<BsonDocument> result)
+        {
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+            result = collection.Find(filter).ToList();
+
+            return (result != null);
+        }
+
+        public bool GetMultipleDatabaseEntries<T>(string field, T value, SortingOrder sortingOrder, string sortingField,
+            out List<BsonDocument> result)
+        {
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+
+            switch (sortingOrder)
+            {
+                case SortingOrder.Ascending:
+                    result = collection.Find(filter).SortBy(bson => bson[sortingField]).ToList();
+                    break;
+                case SortingOrder.Descending:
+                    result = collection.Find(filter).SortByDescending(bson => bson[sortingField]).ToList();
+                    break;
+                default:
+                    GetMultipleDatabaseEntries(field, value, out result);
+                    break;
+            }
+
+            return (result != null);
+        }
+
+        public bool ReplaceSingleDatabaseEntry<T>(string field, T value, BsonDocument entry)
+        {
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+            try
+            {
                 collection.FindOneAndReplace(filter, entry);
             }
-        }
-        catch
-        {
-            return false;
-        }
-        return true;
-    }
-
-    public bool AddSingleDatabaseEntry(BsonDocument entry)
-    {
-        try
-        {
-            collection.InsertOne(entry);
-        }
-        catch
-        {
-            return false;
-        }
-        return true;
-    }
-    public bool AddMultipleDatabaseEntries(List<BsonDocument> entries)
-    {
-        try
-        {
-            collection.InsertMany(entries);
-        }
-        catch
-        {
-            return false;
-        }
-        return true;
-    }
-    
-    public bool RemoveSingleDatabaseEntry<T>(string field, T value)
-    {
-        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
-        try
-        {
-            collection.FindOneAndDelete(filter);
-        }
-        catch
-        {
-            return false;
-        }
-        return true;
-    }
-    public bool RemoveMultiplesDatabaseEntries<T>(string field, T value)
-    {
-        if (!GetMultipleDatabaseEntries(field, value, out List<BsonDocument> result))
-        {
-            return false;
-        }
-        try
-        {
-            foreach (BsonDocument document in result)
+            catch
             {
-                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", document.GetValue("_id"));
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ReplaceMultipleDatabaseEntries<T>(string field, T value, BsonDocument entry)
+        {
+            if (!GetMultipleDatabaseEntries(field, value, out List<BsonDocument> result))
+            {
+                return false;
+            }
+
+            try
+            {
+                foreach (BsonDocument document in result)
+                {
+                    FilterDefinition<BsonDocument> filter =
+                        Builders<BsonDocument>.Filter.Eq("_id", document.GetValue("_id"));
+                    collection.FindOneAndReplace(filter, entry);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool AddSingleDatabaseEntry(BsonDocument entry)
+        {
+            try
+            {
+                collection.InsertOne(entry);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool AddMultipleDatabaseEntries(List<BsonDocument> entries)
+        {
+            try
+            {
+                collection.InsertMany(entries);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool RemoveSingleDatabaseEntry<T>(string field, T value)
+        {
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq(field, value);
+            try
+            {
                 collection.FindOneAndDelete(filter);
             }
+            catch
+            {
+                return false;
+            }
+
+            return true;
         }
-        catch
+
+        public bool RemoveMultiplesDatabaseEntries<T>(string field, T value)
         {
-            return false;
+            if (!GetMultipleDatabaseEntries(field, value, out List<BsonDocument> result))
+            {
+                return false;
+            }
+
+            try
+            {
+                foreach (BsonDocument document in result)
+                {
+                    FilterDefinition<BsonDocument> filter =
+                        Builders<BsonDocument>.Filter.Eq("_id", document.GetValue("_id"));
+                    collection.FindOneAndDelete(filter);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
         }
-        return true;
     }
 }
